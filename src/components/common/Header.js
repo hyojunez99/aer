b0beebda18911f4fba447b0a303b3c2995df089b
hyojunez99 <hyojuez99@gmail.com>
2026-03-24 13:48:41 +0900

    2026.03.24-1

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import LogoImg from "../../assets/images/logo-img.png";
 import "./Header.scss";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <header>
+    <header className={menuOpen ? "active" : ""}>
       <div className="header-inner">
         <h1 className="header-logo">
           <Link to="/">
@@ -12,14 +15,26 @@ const Header = () => {
           </Link>
         </h1>
 
-        <nav className="header-nav">
-          <Link to="/collection">Collection</Link>
-          <Link to="/about">About</Link>
+        {/* 햄버거 */}
+        <button className="menu-btn" onClick={() => setMenuOpen(!menuOpen)}>
+          <span></span>
+          <span></span>
+        </button>
+
+        {/* 모바일 메뉴 */}
+        <nav className={`header-nav ${menuOpen ? "open" : ""}`}>
+          <Link to="/collection" onClick={() => setMenuOpen(false)}>
+            Collection
+          </Link>
+          <Link to="/about" onClick={() => setMenuOpen(false)}>
+            About
+          </Link>
         </nav>
 
         <div className="header-right">
           <Link to="/cart">Cart</Link>
         </div>
+
       </div>
     </header>
   );
